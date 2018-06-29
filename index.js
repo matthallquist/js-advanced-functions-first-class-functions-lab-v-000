1ef28f9b6fc9266c9ba5001ced4fcbad83184f9b
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 16:20:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(array) {
   const result = function(array) {
-    console.log(array[-2,-1]);
+    array[-2,-1];
   }
-  result(array);
+  return result(array);
 }
